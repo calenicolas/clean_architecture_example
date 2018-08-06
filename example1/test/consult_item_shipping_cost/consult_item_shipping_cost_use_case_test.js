b4a5e98b7a1eb52d392a5b01abd9fef2427a8498
Claudio Nicolas Ale <claudio.ale@mercadolibre.com>
2018-08-06 16:46:47 -0300

    tests

---
+++ example1/test/consult_item_shipping_cost/consult_item_shipping_cost_use_case_test.js
@@ -0,0 +1,77 @@
+describe("shows the item's shipping cost for a buyer", function() {
+
+    const should = require('should');
+
+    const ShowItemShippingCostUseCase = require('../../use_cases/show_item_shipping_cost');
+
+    const TestEntityGateway = require('./test_entity_gateway');
+
+    const TestRequest = require('./test_request');
+    const TestPresenter = require('./test_presenter');
+
+    const Buyer = require('../../entities/buyer');
+    const Item = require('../../entities/item');
+
+    it('should show free shipping for buyers with loyalty level 2 and item with price ' +
+        'higher than $1200', function(done) {
+
+        const itemId = 'MLX12345678';
+        const userId = '5678902123';
+
+        const testEntityGateway = new TestEntityGateway();
+        testEntityGateway.mockBuyer(new Buyer());
+        testEntityGateway.mockItem(new Item({
+            id: itemId,
+            price: 1300,
+            shipping_price: 500
+        }));
+
+        const useCase = new ShowItemShippingCostUseCase(
+            testEntityGateway
+        );
+
+        useCase
+            .execute(new TestRequest(itemId, userId), new TestPresenter())
+            .then(renderedUseCaseResult => {
+
+                should(renderedUseCaseResult).be.eql({
+                    item_id: itemId,
+                    shipping_price: 'Es gratis vieja!'
+                });
+
+                done();
+            })
+            .catch(done);
+    });
+
+    it('should show shipping with cost for users with loyalty level 2 and item with price ' +
+        'lower than $1200', function(done) {
+        const itemId = 'MLX12345678';
+        const userId = '5678902123';
+
+        const testEntityGateway = new TestEntityGateway();
+        testEntityGateway.mockBuyer(new Buyer());
+        testEntityGateway.mockItem(new Item({
+            id: itemId,
+            price: 1100,
+            shipping_price: 500
+        }));
+
+        const useCase = new ShowItemShippingCostUseCase(
+            testEntityGateway
+        );
+
+        useCase
+            .execute(new TestRequest(itemId, userId), new TestPresenter())
+            .then(renderedUseCaseResult => {
+
+                should(renderedUseCaseResult).be.eql({
+                    item_id: itemId,
+                    shipping_price: 500
+                });
+
+                done();
+            })
+            .catch(done);
+    });
+});
