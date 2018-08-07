eeda2b4b77447e268f202b20c37f7e07f7b1aabc
Claudio Nicolas Ale <claudio.ale@mercadolibre.com>
2018-08-07 11:40:05 -0300

    rename and interface

---
+++ example1/entities/item/item_shipping_cost/priced_item_shipping_cost.js
@@ -0,0 +1,22 @@
+/**
+ * @implements {ItemShippingCost}
+ * @param {string} itemId
+ * @param {Number} costAsFloat
+ * @constructor
+ */
+function PricedItemShippingCost(itemId, costAsFloat) {
+
+    this._itemId = itemShippingCost;
+    this._costAsFloat = costAsFloat;
+}
+
+/**
+ *
+ * @param {ItemShippingCostRenderer} renderer
+ */
+PricedItemShippingCost.prototype.renderIn = function(renderer){
+
+    return renderer.showItemShippingCost(this._itemId, this._costAsFloat);
+};
+
+module.exports = PricedItemShippingCost;
